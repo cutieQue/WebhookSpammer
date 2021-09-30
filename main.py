7f53c1692bd78aeea60b6e5653f9a82a737f6d47
Que <87747750+cutieQue@users.noreply.github.com>
2021-09-30 23:57:45 +1000

    main.py - fixed UI

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ print(f"{Fore.LIGHTRED_EX}░░╚██╔╝░╚██╔╝░████
 sleep(0.01)
 print(f"{Fore.LIGHTRED_EX}░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝")
 sleep(0.2)
-print(f"{Fore.LIGHTYELLOW_EX}Dev: cutieQue")
+print(f"{Fore.LIGHTYELLOW_EX} Developer: cutieQue")
 sleep(1)
 print(f'{Fore.LIGHTBLUE_EX}\n           [1] Webhook Spammer     [2] Webhook Deleter\n')
 print(f'{Fore.LIGHTRED_EX}[>] {Fore.RESET}', end='')
 choice = int(input(''))
 
 if choice not in [1, 2]:
-    print(f'---\n{Fore.MAGENTA}Option{Fore.RESET} = {Fore.RED}Error{Fore.RESET} : Invalid Choice!')
+    print(f'---\n{Fore.MAGENTA}Option{Fore.RESET} = {Fore.RED}Error{Fore.RESET} : um.. that's an Invalid Choice!')
     time.sleep(1)
     print(f"{Fore.RED}Exiting...")
     time.sleep(3)
