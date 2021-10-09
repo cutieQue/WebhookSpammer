02c81a3b3e9bd6ba8d5385ee70a59b9a84d6b868
Que <87747750+cutieQue@users.noreply.github.com>
2021-10-09 19:59:26 +1100

    More advanced & Better install for v1.1
    installer automatically creates `runSpammer.bat` & is more efficient

--- install.bat
+++ install.bat
@@ -1,2 +1,16 @@
-pip3 install -r requirements.txt
+@echo OFF
+
+ECHO Installing Required Packages for Webhook Spammer
+TIMEOUT 3
+
+py -3 -m pip install -U -r requirements.txt
+
+echo python main.py >> "runSpammer.bat"
+echo pause >> "runSpammer.bat"
+
+echo ---------------------------------------------------------------------------------------------
+
+echo Now Run "runSpammer.bat"!
+echo Report any Errors on the github! https://github.com/cutieQue/Discord-Server-Lookup
+
 pause
